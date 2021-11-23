cfacdccbb153b539e36090e9d1624e6e101dcfaa
Britza <bzegarraflores@danielcastelao.org>
2021-11-23 10:29:27 +0100

    Pagina 404 acabada con estilo

--- html/wp-content/themes/newFuncy/404.php
+++ html/wp-content/themes/newFuncy/404.php
@@ -12,20 +12,21 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>"/>
 
 <h1 style="color: #8524e8; text-align: center">C MAMO JAJAJA C MAMO</h1>
-    <div id="contenedor">
+    <div id="contenedor"class="static">
 
-        <div id="cabecera" class="texto">
+        <div id="cabecera" class="texto" >
             <h1 style="text-align: center">Drake & Josh</h1>
         </div>
 
-        <div id="cuerpo" class="sabroso">
-            <p style="text-align: center">Ja Ja Ja eres un puto payaso <br> NO IORES, NADIE TE VA A DONAR<br> No hay pa vicios, me voy a hacer un porro con tu papel de vistima</p>
+        <div id="cuerpo" class="relative">
+            <p style="font-size: 50px">No hay pa vicios, me voy a hacer un porro con tu papel de vistima</p>
         </div>
 
-        <img class="foto" width="30%" height="50%" src ="<?php echo get_stylesheet_directory_uri();?>/imagenes/icarly-meme.gif">
+        <img class="absoulute" width="30%" height="50%" src ="<?php echo get_stylesheet_directory_uri();?>/imagenes/icarly-meme.gif">
 
     </div>
-
-
+<div id="pie" class="fixed">
+<?php get_footer() ?>
+</div>
 <?php
 
