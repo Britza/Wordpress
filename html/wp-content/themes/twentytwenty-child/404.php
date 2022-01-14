b4a220dbcd760e09b2ff02870ce880b7aeccf482
Britza <bzegarraflores@danielcastelao.org>
2022-01-14 12:33:32 +0100

    Modificacion twenty child (prueba)

---
+++ html/wp-content/themes/twentytwenty-child/404.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the 404 template in the Twenty Twenty theme.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+
+?>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri();?>"/>
+
+<h1 style="color: #8524e8; text-align: center">C MAMO JAJAJA C MAMO</h1>
+    <div id="contenedor"class="static">
+
+        <div id="cabecera" class="texto" >
+            <h1 style="text-align: center">Drake & Josh</h1>
+        </div>
+
+        <div id="cuerpo" class="relative">
+            <p style="font-size: 50px">No hay pa vicios, me voy a hacer un porro con tu papel de vistima</p>
+        </div>
+
+        <img class="absoulute" width="30%" height="50%" src ="<?php echo get_stylesheet_directory_uri();?>/imagenes/icarly-meme.gif">
+
+    </div>
+<div id="pie" class="fixed">
+<?php get_footer() ?>
+</div>
+<?php
+
